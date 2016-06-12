441829151f552fbaac32ef14476144f79bbcd7db
Natasha Hadfield <nmhadfield@dundee.ac.uk>
2016-06-12 22:31:07 +0100

    Close tabs on successful posting of new topic

--- scripts/switch-tab.js
+++ scripts/switch-tab.js
@@ -1,7 +1,16 @@
 function switchTab(evt, key) {
 	var active = evt.currentTarget.className.indexOf("active") >= 0;
 	
-	
+	closeTabs();
+
+	// If the tab was not already active, now show it and tab, and add an "active" class to the link that opened the tab
+	if (!active) {
+		document.getElementById("tab-" + key).style.display = "block";
+		evt.currentTarget.className += " active";
+	}
+}
+
+function closeTabs() {
 	// Get all elements with class="tabcontent" and hide them
 	var tabcontent = document.getElementsByClassName("tabcontent");
 	for (var i = 0; i < tabcontent.length; i++) {
@@ -12,18 +21,11 @@ function switchTab(evt, key) {
 	var tablinks = document.getElementsByClassName("tablinks");
 	for (var i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-
-	// If the tab was not already active, now show it and tab, and add an "active" class to the link that opened the tab
-	if (!active) {
-		document.getElementById("tab-" + key).style.display = "block";
-		evt.currentTarget.className += " active";
-	}
+	}	
 }
 
 jQuery(document).ready(function($) {
     $("#studenthub-subject-select").multiselect({"header": false, "selectedList": 4});
-    
     
     $("#new-post-form").submit(function(event) {
 
@@ -33,5 +35,10 @@ jQuery(document).ready(function($) {
         var url = $form.attr( 'action' );
 
         var posting = $.post( url, $("#new-post-form").serialize() );
-      })
+
+        posting.done(function( data ) {
+        	closeTabs();
+        });
+
+      });
 });
