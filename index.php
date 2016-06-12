d839bbf063c0c9f4faa04155adaab284f1615dd3
Natasha Hadfield <nmhadfield@dundee.ac.uk>
2016-06-12 13:40:12 +0100

    Logo for articles based on categories

--- index.php
+++ index.php
@@ -19,17 +19,22 @@
 	
 	$query = new WP_Query( $bbp_f ); 
 ?>
-	
-	<?php while ( $query->have_posts() ) : $query->the_post(); ?>
 				
 	<div id="content">
 		<div class="blog-holder shadow radius-full post-250 post type-topic status-publish format-standard hentry">
 			<?php locate_template( array( 'activity/post-form.php'), true ); ?>
 		</div>
-		<div class="blog-holder shadow radius-full post-250 post type-topic status-publish format-standard hentry">
+		
+	<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+		<div class="blog-holder shadow radius-full post-250 post type-topic status-publish format-standard hentry">		
 			<div class="article">
-				<p><b><?php the_title(); ?></b><br>
-				<?php the_excerpt(); ?></p>
+				<?php $categories = wp_get_object_terms(get_the_ID(), 'category', array('fields' => 'names')); ?>
+				<?php foreach ($categories as $name) { ?>
+					<img class="logo" src="<?php echo(get_stylesheet_directory_uri()."/img/".$name.".png"); ?>"></img>
+				<?php } ?>
+				
+				<b><?php the_title(); ?></b><br>
+				<?php the_excerpt(); ?>
 				<?php do_action( 'bbp_theme_before_reply_content' ); ?>
 				<?php do_action( 'bbp_theme_after_reply_content' ); ?>
 			</div>
