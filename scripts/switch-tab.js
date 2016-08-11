9376169182ffedf2cade09105a5427aad3578d6a
Natasha Hadfield <nmhadfield@dundee.ac.uk>
2016-08-11 21:19:40 +0100

    Remove search term

--- scripts/switch-tab.js
+++ scripts/switch-tab.js
@@ -133,27 +133,27 @@ function refreshComments() {
 function filterResources(event, category) {
 	// add the newly selected category to the existing search terms
 	var categories = category ? category : "";
-	jQuery(".sh-search-cat input").each(function() {
+	jQuery(".sh-search-cat label").each(function() {
 		if (categories.length) {
 			categories += "+";
 		}
 		categories += jQuery(this).val();
 	});
 	if (category) {
-		jQuery("#sh-search-terms").append("<div class='sh-search-cat'><img src='/images/icons/cross.png'></img><input value='" + category + "'></input></div>");
+		jQuery("#sh-search-terms").append("<div class='sh-search-cat'><a href='' onclick='removeSearchTerm(event)'><img src='/images/icons/cross.png'></img></a><label>" + category+ "</label></div>");
 	}
 	
 	// any search terms within the title or content of a post
 	var term = jQuery("#sh-new-search-term").val();
 	var terms = term ? term : "";
-	jQuery(".sh-search-term input").each(function() {
+	jQuery(".sh-search-term label").each(function() {
 		if (terms.length) {
 			terms += "+";
 		}
 		terms += jQuery(this).val();
 	});
 	if (term) {
-		jQuery("#sh-search-terms").append("<div class='sh-search-term'><img src='/images/icons/cross.png'></img><input value='" + term + "'></input></div>");
+		jQuery("#sh-search-terms").append("<div class='sh-search-term'><img src='/images/icons/cross.png'></img><label>" + term + "</label></div>");
 		jQuery("#sh-new-search-term").val("");
 	}
 	
@@ -166,6 +166,11 @@ function filterResources(event, category) {
 	});
 	
 	return false;
+}
+
+function removeSearchTerm(event) {
+	jQuery(event.currentTarget).parent().remove();
+	filterResources(event);
 }
 
 function feed() {
